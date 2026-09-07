650aa2b4f5a2fd2aa21faebf6c46e8f0ed0d7c5e
atamanahmet <ahmtataman@proton.me>
2026-09-07 15:01:45 +0300

    chore(frontend): switch ThemeProvider to next-themes and add sonner toasts - Wrap app with next-themes class ThemeProvider - Add sonner Toaster component dependency (App already imports it)
    	modified:   frontend/package-lock.json
    	modified:   frontend/package.json
    	new file:   frontend/src/components/ui/sonner.jsx
    	modified:   frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,13 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { createRoot } from "react-dom/client";
 
+import { ThemeProvider } from "next-themes";
 import App from "./App.jsx";
-
-import { ThemeProvider } from "@material-tailwind/react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
 );
